012ec09766bd651fe71f233484a92284c2a28a12
ElfnnXI <ikmalmahdibelajar@gmail.com>
2025-06-28 00:44:42 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace AES_Project
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnEncrypt = new System.Windows.Forms.Button();
+            this.btnDecrypt = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnSelectKeyDir = new System.Windows.Forms.Button();
+            this.lblKeyLocation = new System.Windows.Forms.Label();
+            this.btnDonate = new System.Windows.Forms.Button();
+            this.btnTerms = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEncrypt
+            // 
+            this.btnEncrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnEncrypt.Location = new System.Drawing.Point(17, 347);
+            this.btnEncrypt.Name = "btnEncrypt";
+            this.btnEncrypt.Size = new System.Drawing.Size(402, 60);
+            this.btnEncrypt.TabIndex = 0;
+            this.btnEncrypt.Text = "Encryption";
+            this.btnEncrypt.UseVisualStyleBackColor = false;
+            this.btnEncrypt.Click += new System.EventHandler(this.btnEncrypt_Click);
+            // 
+            // btnDecrypt
+            // 
+            this.btnDecrypt.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDecrypt.Location = new System.Drawing.Point(17, 413);
+            this.btnDecrypt.Name = "btnDecrypt";
+            this.btnDecrypt.Size = new System.Drawing.Size(402, 60);
+            this.btnDecrypt.TabIndex = 1;
+            this.btnDecrypt.Text = "Decryption";
+            this.btnDecrypt.UseVisualStyleBackColor = false;
+            this.btnDecrypt.Click += new System.EventHandler(this.btnDecrypt_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.GreenYellow;
+            this.lblStatus.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatus.Location = new System.Drawing.Point(9, 19);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(115, 13);
+            this.lblStatus.TabIndex = 2;
+            this.lblStatus.Text = "Status: Idle";
+            // 
+            // btnSelectKeyDir
+            // 
+            this.btnSelectKeyDir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnSelectKeyDir.Location = new System.Drawing.Point(12, 479);
+            this.btnSelectKeyDir.Name = "btnSelectKeyDir";
+            this.btnSelectKeyDir.Size = new System.Drawing.Size(820, 60);
+            this.btnSelectKeyDir.TabIndex = 3;
+            this.btnSelectKeyDir.Text = "Save Key";
+            this.btnSelectKeyDir.UseVisualStyleBackColor = false;
+            this.btnSelectKeyDir.Click += new System.EventHandler(this.btnSelectKeyDir_Click);
+            // 
+            // lblKeyLocation
+            // 
+            this.lblKeyLocation.AutoSize = true;
+            this.lblKeyLocation.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.lblKeyLocation.Font = new System.Drawing.Font("OCR A Extended", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblKeyLocation.Location = new System.Drawing.Point(14, 319);
+            this.lblKeyLocation.Name = "lblKeyLocation";
+            this.lblKeyLocation.Size = new System.Drawing.Size(511, 13);
+            this.lblKeyLocation.TabIndex = 4;
+            this.lblKeyLocation.Text = "* Location Key: Log and key storage locations have not been set";
+            // 
+            // btnDonate
+            // 
+            this.btnDonate.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnDonate.Location = new System.Drawing.Point(430, 347);
+            this.btnDonate.Name = "btnDonate";
+            this.btnDonate.Size = new System.Drawing.Size(402, 60);
+            this.btnDonate.TabIndex = 5;
+            this.btnDonate.Text = "Donate";
+            this.btnDonate.UseVisualStyleBackColor = false;
+            this.btnDonate.Click += new System.EventHandler(this.btnDonate_Click);
+            // 
+            // btnTerms
+            // 
+            this.btnTerms.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnTerms.Location = new System.Drawing.Point(430, 413);
+            this.btnTerms.Name = "btnTerms";
+            this.btnTerms.Size = new System.Drawing.Size(402, 60);
+            this.btnTerms.TabIndex = 6;
+            this.btnTerms.Text = "Software Usage Policy";
+            this.btnTerms.UseVisualStyleBackColor = false;
+            this.btnTerms.Click += new System.EventHandler(this.btnTerms_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 561);
+            this.Controls.Add(this.btnTerms);
+            this.Controls.Add(this.btnDonate);
+            this.Controls.Add(this.lblKeyLocation);
+            this.Controls.Add(this.btnSelectKeyDir);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnDecrypt);
+            this.Controls.Add(this.btnEncrypt);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Advanced Encryption Standard";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEncrypt;
+        private System.Windows.Forms.Button btnDecrypt;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnSelectKeyDir;
+        private System.Windows.Forms.Label lblKeyLocation;
+        private System.Windows.Forms.Button btnDonate;
+        private System.Windows.Forms.Button btnTerms;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AES_Project
+{
+    public partial class Form1 : Form
+    {
+        private const string KeyFileName = "key.key";
+        private string keyFolderPath = string.Empty;
+        private string logFilePath = string.Empty;
+        private const string keyFileName = "key.key";
+        private const string logFileName = "log.txt";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Generate a random AES key
+        private byte[] GenerateKey()
+        {
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.GenerateKey();
+                return aes.Key;
+            }
+        }
+
+        // Save key to file
+        private void SaveKey(byte[] key)
+        {
+            string fullKeyPath = Path.Combine(keyFolderPath, keyFileName);
+            File.WriteAllBytes(fullKeyPath, key);
+        }
+
+        // Load key from file
+        private byte[] LoadKey()
+        {
+            string fullKeyPath = Path.Combine(keyFolderPath, keyFileName);
+            return File.ReadAllBytes(fullKeyPath);
+        }
+
+        private void EncryptFile(string filePath, byte[] key)
+        {
+            byte[] iv;
+            byte[] encrypted;
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = key;
+                aes.GenerateIV();
+                iv = aes.IV;
+
+                using (var encryptor = aes.CreateEncryptor())
+                using (var ms = new MemoryStream())
+                {
+                    ms.Write(iv, 0, iv.Length);
+                    using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        byte[] data = File.ReadAllBytes(filePath);
+                        cs.Write(data, 0, data.Length);
+                    }
+                    encrypted = ms.ToArray();
+                }
+            }
+
+            File.WriteAllBytes(filePath, encrypted);
+        }
+
+        private void DecryptFile(string filePath, byte[] key)
+        {
+            byte[] fileData = File.ReadAllBytes(filePath);
+            byte[] iv = new byte[16];
+            Array.Copy(fileData, 0, iv, 0, iv.Length);
+            byte[] encryptedData = new byte[fileData.Length - iv.Length];
+            Array.Copy(fileData, iv.Length, encryptedData, 0, encryptedData.Length);
+
+            using (Aes aes = Aes.Create())
+            {
+                aes.KeySize = 256;
+                aes.Key = key;
+                aes.IV = iv;
+
+                using (var decryptor = aes.CreateDecryptor())
+                using (var ms = new MemoryStream())
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Write))
+                {
+                    cs.Write(encryptedData, 0, encryptedData.Length);
+                    cs.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, ms.ToArray());
+                }
+            }
+        }
+
+        private void EncryptDirectory(string path, byte[] key)
+        {
+            foreach (string file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                if (file.StartsWith(keyFolderPath)) continue; // Hindari enkripsi folder key
+                try
+                {
+                    EncryptFile(file, key);
+                    WriteLog($"Encrypted: {file}");
+                }
+                catch
+                {
+                    WriteLog($"Failed to encrypt: {file}");
+                }
+            }
+        }
+
+        private void DecryptDirectory(string path, byte[] key)
+        {
+            foreach (string file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+            {
+                if (file.StartsWith(keyFolderPath)) continue;
+                try
+                {
+                    DecryptFile(file, key);
+                    WriteLog($"Decrypted: {file}");
+                }
+                catch
+                {
+                    WriteLog($"Failed to decrypt: {file}");
+                }
+            }
+        }
+
+        private void btnEncrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(keyFolderPath))
+            {
+                MessageBox.Show("Please select a folder to save keys and logs.", "Warning");
+                return;
+            }
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] key = File.Exists(Path.Combine(keyFolderPath, keyFileName)) ? LoadKey() : GenerateKey();
+                if (!File.Exists(Path.Combine(keyFolderPath, keyFileName))) SaveKey(key);
+
+                EncryptDirectory(folderBrowserDialog1.SelectedPath, key);
+                lblStatus.Text = "Encryption completed";
+            }
+        }
+
+        private void btnDecrypt_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(keyFolderPath) || !File.Exists(Path.Combine(keyFolderPath, keyFileName)))
+            {
+                MessageBox.Show("Key not found or not selected.", "Error");
+                return;
+            }
+
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] key = LoadKey();
+                DecryptDirectory(folderBrowserDialog1.SelectedPath, key);
+                lblStatus.Text = "Decryption completed";
+            }
+        }
+
+        private void btnSelectKeyDir_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                keyFolderPath = folderBrowserDialog1.SelectedPath;
+                logFilePath = Path.Combine(keyFolderPath, logFileName);
+                lblKeyLocation.Text = "Folder Key: " + keyFolderPath;
+            }
+        }
+
+        private void btnDonate_Click(object sender, EventArgs e)
+        {
+            string message =
+    "Thank you for trying the live demo of this application\n\n" +
+    "I hope you make a small donation for further development of the application by the developer and will be very grateful for it\n\n" +
+    "Here you can send some BTC at this address:1ERMWVmhp1NLkH7JNdh2n3zLtSv7qKrwi8";
+
+            MessageBox.Show(message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnTerms_Click(object sender, EventArgs e)
+        {
+            string terms =
+                "Software Usage Policy\n\n" +
+                "1. The developer is not responsible for any damage to data or files that may occur as a result of using this application.\n\n" +
+                "2. Misuse of this software for illegal actions, hacking, spreading malware, or unlawful purposes is strictly prohibited.\n\n" +
+                "By using this application, you agree to the above policy.";
+
+            MessageBox.Show(terms, "User's Policy", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+        }
+
+        private void WriteLog(string message)
+        {
+            using (StreamWriter sw = File.AppendText(logFilePath))
+            {
+                sw.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+            }
+        }
+    }
+}
